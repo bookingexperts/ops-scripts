ab31a6bdd91b55986391e5e704218a53fe9fad1a
Ruud Seydel <ruud@seydel.me>
2016-08-03 13:28:40 +0200

    Add elastic beanstalk deploy script
    Couldn't find any decent solution to multi env deploys for elastic
    beanstalk so this my take on it.
    
    Will replace the awscli with the ruby sdk later.

---
+++ deploy/elastic_beanstalk.rb
@@ -0,0 +1,103 @@
+# run it remotely with ruby -e "$(curl -fsSL https://raw.github.com/bookingexperts/ops-scripts/master/deploy/elastic_beanstalk.rb)"
+
+require 'date'
+
+# Deploy to AWS ElasticBeanstalk, http://aws.amazon.com/elasticbeanstalk/
+
+# Configuration
+
+%w(
+  aws_default_region
+  aws_secret_access_key
+  aws_access_key_id
+  s3_bucket_name
+  eb_env_names
+  eb_app_name
+).each do |var|
+  if !(value = ENV[var.upcase]).nil?
+    eval "@#{var} = '#{value}'"
+  else
+    raise "You need to configure the #{var.upcase} environment variable!"
+  end
+end
+
+# Generate defaults
+@version_name = [
+  `git rev-parse --short HEAD`.strip,
+  DateTime.now.strftime('%Y-%m-%d_%H_%M_%S')
+].join('_')
+@version_desc = `git show -s --format=%s HEAD`.strip
+@version_file_name = "#{@version_name}.zip"
+@s3_key = "#{@eb_app_name}/#{@version_file_name}"
+
+# Use git archive - not suitable for creating assets while deploying like `rake assets:precompile`
+puts "Zipping your code to #{@version_file_name}"
+`git archive -o "#{@version_file_name}" HEAD`
+
+puts "Uploading to s3://#{@s3_bucket_name}/#{@s3_key}"
+`aws s3 cp #{@version_file_name} s3://#{@s3_bucket_name}/#{@s3_key}`
+
+puts "Create new application version: #{@version_name}"
+`aws elasticbeanstalk create-application-version --application-name #{@eb_app_name} --version-label #{@version_name} --source-bundle S3Bucket=#{@s3_bucket_name},S3Key="#{@s3_key}" --description "#{@version_desc}"`
+
+@failed_deploys = []
+@threads = []
+
+@eb_env_names.split(' ').each do |eb_env_name|
+
+  @threads << Thread.new do
+    puts "#{eb_env_name}: Start update"
+    `aws elasticbeanstalk update-environment --environment-name #{eb_env_name} --version-label #{@version_name}`
+
+    # commands
+
+    env_describe_cmd  = %Q(aws elasticbeanstalk describe-environments --environment-names #{eb_env_name})
+    env_status_cmd    = %Q(#{env_describe_cmd} | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//')
+    env_version_cmd   = %Q(#{env_describe_cmd} | grep VersionLabel | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//')
+    env_color_cmd     = %Q(#{env_describe_cmd} | grep '"Health":' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//')
+
+    puts "#{eb_env_name}: " + `#{env_status_cmd}`.strip
+
+    while `#{env_status_cmd}`.strip == 'Updating' do
+      sleep 5
+    end
+
+    if `#{env_version_cmd}`.strip == @version_name
+      puts "#{eb_env_name}: The version of application code on Elastic Beanstalk matches the version sent in this deployment."
+    else
+      @failed_deploys << eb_env_name
+      puts "#{eb_env_name}: The version of application code on Elastic Beanstalk does not match the version sent in this deployment. Please check your AWS Elastic Beanstalk Console for more information."
+      puts `#{env_describe_cmd}`.strip
+    end
+
+    sleep 5
+
+    tries = 0
+    healthy = false
+    while !(healthy = %w(Green Yellow).include?(`#{env_color_cmd}`.strip)) and tries < 30 do
+      sleep 2
+      tries += 1
+    end
+
+    if healthy
+      puts "#{eb_env_name}: Your environment status is healthy, congrats!"
+    else
+      @failed_deploys << eb_env_name
+      puts "#{eb_env_name}: Your environment status is not healthy, sorry."
+      puts `#{env_describe_cmd}`.strip
+    end
+
+  end
+
+end
+
+# Wait for all the threads to finish
+@threads.each {|tr| tr.join }
+
+if @failed_deploys.empty?
+  puts "All deploys were successfull"
+  exit true
+else
+  puts "The following environments failed to deploy: #{@failed_deploys.uniq.join(', ')}"
+  exit false
+end
