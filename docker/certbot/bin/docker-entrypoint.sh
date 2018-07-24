998fe4a02d57e6f2c491fa35602f04b63749dece
Arthur Holstvoogd <arthurholstvoogd@me.com>
2018-07-24 19:32:07 +0200

    Fix cornjobs

--- docker/certbot/bin/docker-entrypoint.sh
+++ docker/certbot/bin/docker-entrypoint.sh
@@ -6,5 +6,9 @@ trap "{ nginx -s quit }" SIGTERM
 # Start crond
 crond
 
+# It seems starting crond removes these symlinks when build in the dockerfile
+ln -nsf /usr/local/bin/renew /etc/periodic/daily/renew
+ln -nsf /usr/local/bin/rotate_session_keys /etc/periodic/hourly/rotate_session_keys
+
 # Start nginx
 nginx
