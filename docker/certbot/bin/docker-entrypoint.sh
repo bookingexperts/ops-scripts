0eb7b793d076376df6ad056db6bf6f8fc065e057
Arthur Holstvoogd <arthurholstvoogd@me.com>
2018-08-10 15:25:40 +0200

    Fix certbot

--- docker/certbot/bin/docker-entrypoint.sh
+++ docker/certbot/bin/docker-entrypoint.sh
@@ -9,6 +9,7 @@ crond
 # It seems starting crond removes these symlinks when build in the dockerfile
 ln -nsf /usr/local/bin/renew /etc/periodic/daily/renew
 ln -nsf /usr/local/bin/rotate_session_keys /etc/periodic/hourly/rotate_session_keys
+ln -nsf /usr/local/bin/backup /etc/periodic/daily/backup
 
 # Start nginx
 nginx
