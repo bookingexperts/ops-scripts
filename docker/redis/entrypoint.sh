7db89ff0314d5df142b0bc67046f034a96846560
Arthur Holstvoogd <arthurholstvoogd@me.com>
2018-08-26 15:17:55 +0200

    wip: dedicated sentinels

--- docker/redis/entrypoint.sh
+++ docker/redis/entrypoint.sh
@@ -1,55 +1,47 @@
 #!/bin/sh
-function set_master {
-  master="$(redis-cli -h sentinel.ecs.be.internal -p 26379 sentinel get-master-addr-by-name queue-master || echo 'Connection refused')"
-  master=$(echo "$master" | tr '\n' ' ')
+set -ex
+
+function main {
+  if [ "$(get_sentinels)" == "" ]; then
+    redis-sentinel /etc/redis/sentinel.conf
+  else
+    join_cluster && redis-sentinel /etc/redis/sentinel.conf
+  fi
 }
 
-function deregister {
-  # if master -> failover
-  # reset sentinels
+function get_master {
+  master="$(redis-cli -h sentinel.be.internal -p 26379 sentinel get-master-addr-by-name queue-master || echo 'Connection refused')"
+  echo "$master" | tr '\n' ' '
 }
 
-mode=$1
-shift
-case $mode in
-sentinel)
+# Get other sentinel ip adresses fron target group
+function get_sentinels {
+  aws elbv2 describe-target-health --output text --target-group-arn $TARGET_GROUP_ARN | grep -v $(hostname -i) | egrep -o '(\d{1,3}\.){3}\d{1,3}'
+}
+
+# Resets all _other_ sentinels
+function reset_sentinels {
+  for host in $(get_sentinels); do
+    redis-cli -h $host -p 26379 sentinel reset *
+    sleep 30
+  done
+}
+
+function join_cluster {
   while true; do
-    set_master
+    master=$(get_master)
     case "$master" in
-    *"Connection refused"*)
-      echo "No active sentinels found, starting..."
-      break
-      ;;
-    "")
-      echo "Waiting for master to be registered..."
-      sleep 5
-      ;;
     *"6379"*)
+      echo "Master found: ${master}. Join cluster..."
       echo "sentinel monitor queue-master ${master} 2" >> /etc/redis/sentinel.conf
+      reset_sentinels
       break
       ;;
     *)
-      echo "Something went wrong... This is the master:"
-      echo $master
-      break
+      sleep 5
       ;;
     esac
   done
-  redis-sentinel /etc/redis/sentinel.conf ${@}
-  ;;
-queue)
-  set_master
-  case "$master" in
-  *"Connection refused"*|"")
-    echo "No master found, starting as master..."
-    ;;
-  *"6379"*)
-    echo "slaveof ${master}" >> /etc/redis/queue.conf
-    ;;
-  esac
-  redis-server /etc/redis/queue.conf ${@}
-  ;;
-*)
-  echo "Invalid mode. Usage $0 (sentinel|queue)"
-  ;;
-esac
+}
+
+main
