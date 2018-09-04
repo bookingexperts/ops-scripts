92873bc1900a024d551fbb17ace30a1730bc5baa
Arthur Holstvoogd <arthurholstvoogd@me.com>
2018-09-04 18:46:00 +0200

    Remove old redis stuff, rename to sentinel; fix dns validation, improve be-certbot; reduce app-base GC settings

--- docker/redis/init-master.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cluster="be-ecs-cluster"
-instances=$(aws --output text ecs list-container-instances --cluster $cluster | awk '{print $2}' | tr '\n' ' ')
-instance_ids=$(aws --output text ecs describe-container-instances --container-instances $instances --cluster $cluster --query 'containerInstances[*].[ec2InstanceId]')
-echo $instance_ids
